69a47c6a3384090ad6baf7947140553b2daf98f9
Nerogar <nerogar@arcor.de>
2023-04-12 16:04:27 +0200

    add a new GenerateImageLike module

--- src/mgds/GenericDataLoaderModules.py
+++ src/mgds/GenericDataLoaderModules.py
@@ -266,11 +266,52 @@ class LoadImage(PipelineModule):
     def get_item(self, index: int, requested_name: str = None) -> dict:
         path = self.get_previous_item(self.path_in_name, index)
 
-        image_tensor = Image.open(path)
-        image_tensor = image_tensor.convert(self.mode)
+        image = Image.open(path)
+        image = image.convert(self.mode)
 
         t = transforms.ToTensor()
-        image_tensor = t(image_tensor).to(device=self.pipeline.device, dtype=self.pipeline.dtype)
+        image_tensor = t(image).to(device=self.pipeline.device, dtype=self.pipeline.dtype)
+
+        image_tensor = image_tensor * (self.range_max - self.range_min) + self.range_min
+
+        return {
+            self.image_out_name: image_tensor
+        }
+
+
+class GenerateImageLike(PipelineModule):
+    def __init__(self, image_in_name: str, image_out_name: str, color: float | int | tuple[float, float, float], range_min: float, range_max: float, channels: int = 3):
+        super(GenerateImageLike, self).__init__()
+        self.image_in_name = image_in_name
+        self.image_out_name = image_out_name
+        self.color = color
+
+        self.range_min = range_min
+        self.range_max = range_max
+
+        if channels == 3 and isinstance(color, tuple):
+            self.mode = 'RGB'
+        elif channels == 1 and (isinstance(color, float) or isinstance(color, int)):
+            self.mode = 'L'
+        else:
+            raise ValueError('Only 1 and 3 channels are supported.')
+
+    def length(self) -> int:
+        return self.get_previous_length(self.image_in_name)
+
+    def get_inputs(self) -> list[str]:
+        return []
+
+    def get_outputs(self) -> list[str]:
+        return [self.image_out_name]
+
+    def get_item(self, index: int, requested_name: str = None) -> dict:
+        original_image = self.get_previous_item(self.image_in_name, index)
+
+        image = Image.new(mode=self.mode, size=(original_image.shape[2], original_image.shape[1]), color=self.color)
+
+        t = transforms.ToTensor()
+        image_tensor = t(image).to(device=self.pipeline.device, dtype=self.pipeline.dtype)
 
         image_tensor = image_tensor * (self.range_max - self.range_min) + self.range_min
 
