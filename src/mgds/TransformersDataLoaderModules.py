d8d6018a49b1b0c760cc0139cd428c262d787f01
Nerogar <nerogar@arcor.de>
2023-11-16 22:07:55 +0100

    add text encoder layer norm option

--- src/mgds/TransformersDataLoaderModules.py
+++ src/mgds/TransformersDataLoaderModules.py
@@ -118,6 +118,7 @@ class EncodeClipText(PipelineModule):
             hidden_state_out_name: str,
             pooled_out_name: str | None,
             text_encoder: CLIPTextModel | CLIPTextModelWithProjection,
+            add_layer_norm: bool,
             hidden_state_output_index: int | None = None,
             override_allow_mixed_precision: bool | None = None,
     ):
@@ -126,6 +127,7 @@ class EncodeClipText(PipelineModule):
         self.hidden_state_out_name = hidden_state_out_name
         self.pooled_out_name = pooled_out_name
         self.text_encoder = text_encoder
+        self.add_layer_norm = add_layer_norm
         self.hidden_state_output_index = hidden_state_output_index
         self.override_allow_mixed_precision = override_allow_mixed_precision
 
@@ -164,6 +166,12 @@ class EncodeClipText(PipelineModule):
         pooled_state = None if pooled_state is None else pooled_state.squeeze()
 
         hidden_state = hidden_states[self.hidden_state_output_index]
+
+        if self.add_layer_norm:
+            final_layer_norm = self.text_encoder.text_model.final_layer_norm
+            hidden_state = final_layer_norm(
+                hidden_state
+            )
 
         return {
             self.hidden_state_out_name: hidden_state,
