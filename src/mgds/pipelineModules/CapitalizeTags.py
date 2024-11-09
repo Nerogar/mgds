f9edb99bea18da54440c4600894027706b5172ce
efhosci <109105900+efhosci@users.noreply.github.com>
2024-11-09 11:58:18 -0800

    Text Augmentation additions (#9)
    Add two new modules for modifying text in captions:
    
    CapitalizeTags: Will randomly capitalize tags in the caption with a set probability. Can specify comma-separated list of capitalization modes - 'capslock' (ALL CAPS), 'title' (First Letter Of Every Word), 'first' (First word only), or 'random' (rAnDOm lETteRS) and it will iterate through each tag in the caption and apply a random mode to each. Also has an option to set the entire caption to lowercase first. Uses the same delimiter as defined for the existing "shuffle tags" option.
    
    DropTags: Will randomly remove tags from the caption with a specified probability. Also currently uses the same existing "keep tags" and "delimiter" options. Has three different modes of dropping tags:
       - FULL - all-or-nothing, will drop everything after the 'keep tags' amount
       - RANDOM - will iterate through the caption and randomly drop individual tags
       - RANDOM WEIGHTED - same as random, but the probability to drop tags is reduced at the start of the caption and only reaches the full value at the end, making it more likely to preserve tags at the start
    
    Also supports a "special tags" list, which can be either a delimiter-separated string in the input field or a file path to a txt or csv file with entries separated by new lines. Can act as a "whitelist" (the special tags will never be dropped, but all others past the "keep tags" amount can be) or "blacklist" (the special tags may be dropped but all others will always be kept). There's also an option to enable this list to be interpreted with regex matching, so that syntax like ".*s" will match any tags ending with the letter s. Includes an exception for the "/(" and "/)" syntax used in many booru/e6 tags, but there may be some other special characters used by regex "[]\.^$*+?{}|()" which could be interpreted in unintentional ways.

---
+++ src/mgds/pipelineModules/CapitalizeTags.py
@@ -0,0 +1,75 @@
+from mgds.PipelineModule import PipelineModule
+from mgds.pipelineModuleTypes.RandomAccessPipelineModule import RandomAccessPipelineModule
+import string
+
+
+class CapitalizeTags(
+    PipelineModule,
+    RandomAccessPipelineModule,
+):
+    def __init__(
+            self,
+            text_in_name: str,
+            enabled_in_name: str,
+            probability_in_name: str,         #from 0-1, probability capitalization adjustment will be applied to each tag
+            capitalize_mode_in_name: str,       #comma-separated list of which caps changes can be applied, accepts "capslock, title, first, random"
+            delimiter_in_name: str,
+            convert_lowercase_in_name: str,     #whether to apply lowercase to entire caption before any other caps changes
+            text_out_name: str,
+    ):
+        super(CapitalizeTags, self).__init__()
+        self.text_in_name = text_in_name
+        self.enabled_in_name = enabled_in_name
+        self.probability_in_name = probability_in_name
+        self.capitalize_mode_in_name = capitalize_mode_in_name
+        self.delimiter_in_name = delimiter_in_name
+        self.convert_lowercase_in_name = convert_lowercase_in_name
+        self.text_out_name = text_out_name
+
+    def length(self) -> int:
+        return self._get_previous_length(self.text_in_name)
+
+    def get_inputs(self) -> list[str]:
+        return [self.text_in_name, self.enabled_in_name, self.probability_in_name, self.capitalize_mode_in_name, self.delimiter_in_name, self.convert_lowercase_in_name]
+
+    def get_outputs(self) -> list[str]:
+        return [self.text_out_name]
+
+    def get_item(self, variation: int, index: int, requested_name: str = None) -> dict:
+        text = self._get_previous_item(variation, self.text_in_name, index)
+        delimiter = self._get_previous_item(variation, self.delimiter_in_name, index)
+        enabled = self._get_previous_item(variation, self.enabled_in_name, index)
+        probability = self._get_previous_item(variation, self.probability_in_name, index)
+        capitalize_mode = self._get_previous_item(variation, self.capitalize_mode_in_name, index)
+        convert_lowercase = self._get_previous_item(variation, self.convert_lowercase_in_name, index)
+        rand = self._get_rand(variation, index)
+
+        #if convert_lower is enabled always convert caption to lowercase before any other changes
+        if convert_lowercase:
+            text = text.lower()
+
+        #randomly pick one of the capitalization methods for each tag
+        if enabled and (probability > 0):
+            text_list = []
+            for s in [tag.strip() for tag in text.split(delimiter)]:
+                if rand.random() < probability:
+                    capmode_list = [tag.strip() for tag in capitalize_mode.split(",")]
+                    match rand.choice(capmode_list):
+                        case "capslock":  #make ALL CAPS
+                            text_list.append(s.upper())
+                        case "title":     #make First Letter Of All Words Caps
+                            text_list.append(string.capwords(s))
+                        case "first":     #make First word only caps
+                            text_list.append(s.capitalize())
+                        case "random":    #make rAnDOm lETTerS CaPs
+                            s2 = ''.join(rand.choice(x) for x in zip(s.upper(), s.lower()))
+                            text_list.append(s2)
+                        case _:           #catch any unexpected values
+                            text_list.append(s)
+                else:
+                    text_list.append(s)
+            text = delimiter.join(text_list)
+ 
+        return {
+            self.text_out_name: text
+        }
---
+++ src/mgds/pipelineModules/DropTags.py
@@ -0,0 +1,167 @@
+from mgds.PipelineModule import PipelineModule
+from mgds.pipelineModuleTypes.RandomAccessPipelineModule import RandomAccessPipelineModule
+import csv
+import re
+import os
+import functools
+
+
+class DropTags(
+    PipelineModule,
+    RandomAccessPipelineModule,
+):
+    def __init__(
+            self,
+            text_in_name: str,
+            enabled_in_name: str,
+            probability_in_name: str,
+            dropout_mode_in_name: str,
+            special_tags_in_name: str,
+            special_tag_mode_in_name: str,
+            delimiter_in_name: str,
+            regex_enabled_in_name: str,
+            keep_tags_count_in_name: str,
+            text_out_name: str,
+    ):
+        super(DropTags, self).__init__()
+        self.text_in_name = text_in_name
+        self.enabled_in_name = enabled_in_name
+        self.probability_in_name = probability_in_name
+        self.dropout_mode_in_name = dropout_mode_in_name
+        self.special_tags_in_name = special_tags_in_name
+        self.special_tag_mode_in_name = special_tag_mode_in_name
+        self.delimiter_in_name = delimiter_in_name
+        self.regex_enabled_in_name = regex_enabled_in_name
+        self.keep_tags_count_in_name = keep_tags_count_in_name
+        self.text_out_name = text_out_name
+
+    def length(self) -> int:
+        return self._get_previous_length(self.text_in_name)
+
+    def get_inputs(self) -> list[str]:
+        return [self.text_in_name,
+                self.enabled_in_name,
+                self.probability_in_name,
+                self.dropout_mode_in_name,
+                self.special_tags_in_name,
+                self.special_tag_mode_in_name,
+                self.delimiter_in_name,
+                self.regex_enabled_in_name,
+                self.keep_tags_count_in_name]
+
+    def get_outputs(self) -> list[str]:
+        return [self.text_out_name]
+    
+    #convert special_tags to list depending on whether it's a newline-separated csv/txt file
+    # or a delimiter-separated string
+    #cached to reduce file read operations
+    @functools.lru_cache
+    def get_special_tags(self, sptags: str, delim: str) -> list[str]:
+        if sptags.endswith(".txt") and os.path.isfile(sptags):
+            with open(sptags) as special_tags_file:
+                return [line.rstrip('\n') for line in special_tags_file]
+        elif sptags.endswith(".csv") and os.path.isfile(sptags):
+            with open(sptags, 'r') as special_tags_file:
+                splist = []
+                for row in csv.reader(special_tags_file):
+                    splist.append(row[0])
+                return splist
+        else:
+            return [tag.strip() for tag in sptags.split(delim)]
+
+    #parse regex expressions, create new special list based on matches   
+    def parse_regex(self, splist_in: list[str], taglist: list[str]) -> list[str]:
+        splist_out = []
+        regex_spchars = set(".^$*+?!\{\}\[\]|()\\")
+        for c in splist_in:
+            if any((a in regex_spchars) for a in c):    #only do regex matching if tag contains special character
+                c = c.replace("\)", "\\\\\)")
+                c = c.replace("\(", "\\\\\(")   #hopefully fix issues caused by "\(\)" syntax without affecting other regex
+                r = re.compile(c)
+                for s in taglist:
+                    if r.fullmatch(s):
+                        splist_out.append(s)
+            else:
+                splist_out.append(c)
+        return splist_out    
+    
+    #change probability evaluated against random() depending on mode
+
+    def probability_weighted(self, p: float, mode: str, i: int, input_length: int) -> float:
+        if mode == "RANDOM":
+            return p
+        elif mode == "RANDOM WEIGHTED":
+            return p * (i / input_length)
+        else:  # if unexpected "mode" given default to return p directly
+            return p
+
+    def get_item(self, variation: int, index: int, requested_name: str = None) -> dict:
+        text = self._get_previous_item(variation, self.text_in_name, index)
+        delimiter = self._get_previous_item(variation, self.delimiter_in_name, index)
+        keep_tags_count = self._get_previous_item(variation, self.keep_tags_count_in_name, index)
+        enabled = self._get_previous_item(variation, self.enabled_in_name, index)
+        probability = self._get_previous_item(variation, self.probability_in_name, index)
+        dropout_mode = self._get_previous_item(variation, self.dropout_mode_in_name, index)
+        special_tag_mode = self._get_previous_item(variation, self.special_tag_mode_in_name, index)
+        special_tags = self._get_previous_item(variation, self.special_tags_in_name, index)
+        regex_enabled = self._get_previous_item(variation, self.regex_enabled_in_name, index)
+        rand = self._get_rand(variation, index)
+
+        if enabled and (probability > 0):
+            #convert inputs to lists and set up final output list (pruned)
+            tags = [tag.strip() for tag in text.split(delimiter)]
+            keep_tags = tags[:keep_tags_count]
+            dropout_tags = tags[keep_tags_count:]
+            pruned_tags = []
+            
+            #load special tags from file or string
+            special_tags_prelist = self.get_special_tags(special_tags, delimiter)
+
+            #match any regex expressions in the special tags prelist to tags in the dropout list
+            # and create a new list of all matching tags
+            #if any sort of (tag weighting:1.2) or {other|special|syntax} is added in the future
+            # this may need to be changed
+            if regex_enabled:
+                special_tags_list = self.parse_regex(special_tags_prelist, dropout_tags)
+            else:
+                special_tags_list = special_tags_prelist
+
+            #remove duplicates
+            special_tags_list = list(set(special_tags_list))
+
+            if dropout_mode == "FULL" and rand.random() < probability:
+                #keep only whitelist tags if random < probability, or any non-blacklist tags
+                for s in dropout_tags:
+                    if special_tag_mode == "WHITELIST" and s in special_tags_list:
+                        pruned_tags.append(s)
+                    elif special_tag_mode == "BLACKLIST" and not(s in special_tags_list):
+                        pruned_tags.append(s)
+                    else:   #NONE or any other unexpected values
+                        pruned_tags.append(s)
+            elif dropout_mode.startswith("RANDOM"):     
+                #iterate through dropout_tags and add to pruned_tags if random > probability
+                if special_tag_mode == "WHITELIST":
+                    for i, s in enumerate(dropout_tags):
+                        if rand.random() > self.probability_weighted(probability, dropout_mode, i, len(dropout_tags)) \
+                        or (s in special_tags_list):
+                            pruned_tags.append(s)
+                elif special_tag_mode == "BLACKLIST":
+                    for i, s in enumerate(dropout_tags):
+                        if rand.random() > self.probability_weighted(probability, dropout_mode, i, len(dropout_tags)) \
+                        or not(s in special_tags_list):
+                            pruned_tags.append(s)
+                else:   #NONE or any other unexpected values
+                    for i, s in enumerate(dropout_tags):
+                        if rand.random() > self.probability_weighted(probability, dropout_mode, i, len(dropout_tags)):
+                            pruned_tags.append(s)
+            else:
+                #avoid dropping any captions if dropout_mode isn't an expected value,
+                # or if in "FULL" mode and random > probability
+                pruned_tags = dropout_tags
+
+            tags = keep_tags + pruned_tags
+            text = delimiter.join(tags)
+
+        return {
+            self.text_out_name: text
+        }
