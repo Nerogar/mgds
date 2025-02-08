8aeb53c9806e1900c7e2c0963c731b8e649eb55c
Nerogar <nerogar@arcor.de>
2025-02-09 00:42:28 +0100

    speed up video duration calculation for most video formats

--- src/mgds/pipelineModules/LoadVideo.py
+++ src/mgds/pipelineModules/LoadVideo.py
@@ -5,6 +5,7 @@ import torch
 from mgds.PipelineModule import PipelineModule
 from mgds.pipelineModuleTypes.RandomAccessPipelineModule import RandomAccessPipelineModule
 
+AV_TIME_BASE = 1_000_000
 
 class LoadVideo(
     PipelineModule,
@@ -51,16 +52,21 @@ class LoadVideo(
 
         video_stream = container.streams.video[0]
 
+        frame_rate = video_stream.base_rate.numerator / video_stream.base_rate.denominator
         if video_stream.frames > 0:
             frame_count = video_stream.frames
+        elif container.duration > 0:
+            frame_count = int(container.duration / AV_TIME_BASE * frame_rate)
+        elif 'DURATION' in video_stream.metadata:
+            metadata_duration_frames = [frame_rate, frame_rate * 60, frame_rate * 60 * 60]
+            metadata_duration = reversed([float(x) for x in video_stream.metadata['DURATION'].split(':')])
+            frame_count = int(sum(d * f for d, f in zip(metadata_duration, metadata_duration_frames, strict=False)))
         else:
             print(f"could not find length of video, falling back to full decode {path}")
             # fall back to counting frames (and hope the user didn't specify a long video)
             decoded = container.decode(video=0)
 
             frame_count = sum(1 for _ in decoded)
-
-        frame_rate = video_stream.base_rate.numerator / video_stream.base_rate.denominator
 
         return frame_count, frame_rate
 
@@ -94,8 +100,11 @@ class LoadVideo(
 
                 frames = []
                 while len(frames) < target_frame_count:
-                    frame = decoded.__next__()
-                    frames.append(torch.from_numpy(frame.to_rgb().to_ndarray()).movedim(2, 0))
+                    frame = next(decoded, None)
+                    if frame is None:
+                        frames.append(frames[-1])
+                    else:
+                        frames.append(torch.from_numpy(frame.to_rgb().to_ndarray()).movedim(2, 0))
 
                 video_tensor = torch.stack(frames, dim=1)
                 del frames
