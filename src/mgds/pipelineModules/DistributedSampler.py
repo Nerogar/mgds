9d29c1e1e5b55e055d3b05f202331e8caedd57a1
dxqbYD <dxqbYD@outlook.de>
2025-04-20 21:07:05 +0200

    sampling for distributed training

---
+++ src/mgds/pipelineModules/DistributedSampler.py
@@ -0,0 +1,33 @@
+from mgds.PipelineModule import PipelineModule
+from mgds.pipelineModuleTypes.SingleVariationRandomAccessPipelineModule import SingleVariationRandomAccessPipelineModule
+
+
+class DistributedSampler(
+    PipelineModule,
+    SingleVariationRandomAccessPipelineModule,
+):
+    def __init__(self, names: [str], local_batch_size: int, world_size: int, rank: int):
+        super(DistributedSampler, self).__init__()
+        self.names = names
+        self.local_batch_size = local_batch_size
+        self.world_size = world_size
+        self.rank = rank
+
+
+    def length(self) -> int:
+        print(f"length input: {self._get_previous_length(self.names[0])}, length output: {self._get_previous_length(self.names[0]) // self.world_size}")
+        return self._get_previous_length(self.names[0]) // self.world_size
+
+    def get_inputs(self) -> list[str]:
+        return self.names
+
+    def get_outputs(self) -> list[str]:
+        return self.names
+
+    def get_item(self, index: int, requested_name: str = None) -> dict:
+        prev_index = (index * self.world_size) + self.rank
+        print(f"Distributed sampler, prev_index: {prev_index}, index: {index}")
+        item = {}
+        for name in self.names:
+            item[name] = self._get_previous_item(self.current_variation, name, prev_index)
+        return item
