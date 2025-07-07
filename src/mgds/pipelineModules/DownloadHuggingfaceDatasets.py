acc97a5d174b9da5d7b326ded255742caace47c9
dxqbYD <dxqbYD@outlook.de>
2025-07-07 14:46:33 +0200

    changes based on PR comments

--- src/mgds/pipelineModules/DownloadHuggingfaceDatasets.py
+++ src/mgds/pipelineModules/DownloadHuggingfaceDatasets.py
@@ -3,6 +3,7 @@ import os
 from mgds.PipelineModule import PipelineModule
 from mgds.pipelineModuleTypes.RandomAccessPipelineModule import RandomAccessPipelineModule
 import huggingface_hub
+from contextlib import suppress
 
 
 class DownloadHuggingfaceDatasets(
@@ -11,7 +12,9 @@ class DownloadHuggingfaceDatasets(
 ):
     def __init__(
             self,
-            concept_in_name: str, path_in_name: str, enabled_in_name: str,
+            concept_in_name: str,
+            path_in_name: str,
+            enabled_in_name: str,
             concept_out_name: str,
     ):
         super(DownloadHuggingfaceDatasets, self).__init__()
@@ -39,13 +42,18 @@ class DownloadHuggingfaceDatasets(
             enabled = concept[self.enabled_in_name]
             if enabled:
                 path = concept[self.path_in_name]
+
                 is_local = os.path.isdir(path)
                 if not is_local:
-                    concept[self.path_in_name] = huggingface_hub.snapshot_download(
-                        repo_id=path,
-                        repo_type="dataset",
-                        max_workers=16,
-                    )
+                    with suppress(huggingface_hub.errors.HFValidationError):
+                        hf_path = huggingface_hub.snapshot_download(
+                            repo_id=path,
+                            repo_type="dataset",
+                            max_workers=16,
+                        )
+                        concept = concept.copy()
+                        concept[self.path_in_name] = hf_path
+
                 self.concepts.append(concept)
 
     def get_item(self, variation: int, index: int, requested_name: str = None) -> dict:
